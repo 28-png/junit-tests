aa2864352bdeb0dd34da9da866705624e1045bd9
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-02-14 10:10:03 -0600

    updated

--- src/test/java/Student.java
+++ src/test/java/Student.java
@@ -1,2 +1,23 @@
+import java.util.ArrayList;
+
+
+
 public class Student {
+    private long id;
+    private String name;
+    private ArrayList<Integer> grades;
+
+    public Student(long id, String name, ArrayList<Integer> grades) {
+        this.id = id;
+        this.name = name;
+        this.grades = grades;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+
+
+
 }
