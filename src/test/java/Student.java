68a8b3711e9d17d1b98f1604883da012e45247a5
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-02-14 11:36:22 -0600

    updated

--- src/test/java/Student.java
+++ src/test/java/Student.java
@@ -3,31 +3,31 @@ import java.util.Arrays;
 
 
 public class Student {
-    private long id;
-    private String name;
-    private ArrayList<Integer> grades;
-
-    public Student(long id, String name, ArrayList<Integer> grades) {
-        this.id = id;
-        this.name = name;
-        this.grades = grades;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void addGrade(int grade) {
-        grades.add(grade);
-    }
-
-    public double getAverage(int[] grades) {
-        return Arrays.stream(grades).average().orElse(Double.NaN);
-    }
+//    private long id;
+//    private String name;
+//    private ArrayList<Integer> grades;
+//
+//    public Student(long id, String name, ArrayList<Integer> grades) {
+//        this.id = id;
+//        this.name = name;
+//        this.grades = grades;
+//    }
+//
+//    public long getId() {
+//        return id;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void addGrade(int grade) {
+//        grades.add(grade);
+//    }
+//
+//    public double getGradeAverage(int[] grades) {
+//        return Arrays.stream(grades).average().orElse(Double.NaN);
+//    }
 
 
 }
--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,4 +1,24 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
 public class StudentTest {
+   private ArrayList<Integer> grades;
+
+    @Before
+    public void setUp() {
+        this.grades = new ArrayList<>();
+
+    }
+
+    @Test
+    public void testIfArrayList() {
+        int[] expected = {70, 80, 90, 100};
+        int[] output = {70, 80, 90, 100};
+        assertArrayEquals(expected, output);
+    }
 
 
 
